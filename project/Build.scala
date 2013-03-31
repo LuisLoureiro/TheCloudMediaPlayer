8926a6e550c8c929f554c471c8c975ed0abf1099
Luís Loureiro <llorieruo@hotmail.com>
2013-03-31 19:34:26 +0100

    Updating project managed dependencies to be able to use the Google API client library for Java.

--- project/Build.scala
+++ project/Build.scala
@@ -11,11 +11,19 @@ object ApplicationBuild extends Build {
     // Add your project dependencies here,
     javaCore,
     javaJdbc,
-    javaEbean
+    javaEbean,
+    // groupID % artifactID % revision % configuration
+    "com.google.api-client" % "google-api-client" % "1.14.1-beta",
+    "com.google.http-client" % "google-http-client-jackson" % "1.14.1-beta",
+    "com.google.http-client" % "google-http-client-gson" % "1.14.1-beta",
+    "com.google.apis" % "google-api-services-oauth2" % "v2-rev32-1.14.1-beta"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+    // Add your own project settings here
+      resolvers += (
+          "Google api services" at "http://google-api-client-libraries.appspot.com/mavenrepo"
+      )
   )
 
 }
