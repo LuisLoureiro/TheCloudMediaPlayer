5661cfd08c946e9fbc3e434a93cbae325f16b210
Luís Loureiro <llorieruo@hotmail.com>
2013-04-01 22:28:12 +0100

    Better organization of code with new functions. Getting user id to send to the server for better validation of the server access token.

--- public/javascripts/google-functions.js
+++ public/javascripts/google-functions.js
@@ -1,11 +1,11 @@
 // Place this asynchronous JavaScript just before your </body> tag
 (function() {
     var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-    po.src = 'https://apis.google.com/js/client:plusone.js?onload=start';
+    po.src = 'https://apis.google.com/js/client:plusone.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
 })();
 var googleHelper = (function() {
-	var clientAccessToken = undefined;
+	var authResult = undefined;
 	return {
 		// Helper method that handles the authentication flow.
 		signInCallback: function(authResult) {
@@ -18,35 +18,11 @@ var googleHelper = (function() {
 //			  "error": The OAuth2 error type if problems occurred,
 //			  "error_description": an error message if problems occurred
 //			}
-			if (authResult['code']) {
+			if (authResult.access_token) {
 		        // The user is signed in. Save the access token to be used in the client side.
-		        this.clientAccessToken = authResult['access_token'];
-			    // Send the code to the server
-			    $.ajax({
-			    	type: 'POST',
-			    	url: '/auth/exchangecode',
-				    contentType: "application/x-www-form-urlencoded; charset=UTF-8",
-			    	success: function(result) {
-//			    		// Handle or verify the server response if necessary.
-//			    		// Prints the list of people that the user has allowed the app to know
-//			    		// to the console.
-//			    		console.log(result);
-//			    		if (result['profile'] && result['people']){
-//			    			$('#results').html('Hello ' + result['profile']['displayName'] + '. You successfully made a server side call to people.get and people.list');
-//			    		} else {
-//			    			$('#results').html('Failed to make a server-side call. Check your configuration and console.');
-//			    		}
-			    		window.location.assign('/user/google');
-			    	},
-			    	data: {
-			    		'code': authResult['code'],
-//			    		'csrf': 
-			    	},
-				    error: function(e) {
-				        // Handle the error
-				        console.log(e);
-				    }
-			    });
+		        this.authResult = authResult;
+		        // After we load the Google+ API, render the user info from Google+.
+		        gapi.client.load('plus','v1',this.renderUserInfo);
 			} else if (authResult['error']) {
 			    // There was an error.
 			    // Possible error codes:
@@ -55,29 +31,56 @@ var googleHelper = (function() {
 			    console.log('There was an error("' + authResult['error'] + '"): ' + authResult['error_description']);
 			}
 		},
+		renderUserInfo: function() {
+			var request = gapi.client.plus.people.get({'userId': 'me'});
+	    	request.execute(function(profile) {
+	    		if (profile.error) {
+	    			appendErrorAlert(profile.error);
+	    			return;
+	    		}
+			    // Send the code to the server
+	    		googleHelper.serverExchangeCode(googleHelper.authResult.code, profile.id);
+//	    		$('#profile').append($('<p><img src=\"' + profile.image.url + '\"></p>'));
+//	    		$('#profile').append($('<p>Hello ' + profile.displayName + '!<br />Tagline: ' + profile.tagline + '<br />About: ' + profile.aboutMe + '</p>'));
+//	    		if (profile.cover && profile.coverPhoto) {
+//	    			$('#profile').append($('<p><img src=\"' + profile.cover.coverPhoto.url + '\"></p>'));
+//	    		}
+	        });
+		},
+		serverExchangeCode: function(authorizationCode, userId) {
+			$.ajax({
+		    	type: 'POST',
+		    	url: '/auth/exchangecode?provider=google',
+			    contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+		    	success: function() {
+		    		window.location.assign('/user?provider=google');
+		    	},
+		    	data: {
+		    		'code': authorizationCode,
+		    		'userId': userId
+//		    		'csrf': 
+		    	},
+			    error: function(e) {
+			        // Handle the error
+			        console.log(e);
+			    }
+		    });
+		},
 		disconnectUser: function() {
 			var revokeUrl = 'https://accounts.google.com/o/oauth2/revoke?token=' + this.clientAccessToken;
-			// Perform an asynchronous GET request.
 			$.ajax({
 			    type: 'GET',
 			    url: revokeUrl,
-			    async: false,
 			    contentType: "application/json",
 			    dataType: 'jsonp',
 			    success: function(nullResponse) {
 			        // Do something now that user is disconnected
 			        // The response is always undefined.
-				    // Hide the sign-out button now that the user is disconnected:
-//				    document.getElementById('revokeButton').setAttribute('style', 'display: none');
-//				    // Show the sign-in button
-//				    document.getElementById('signinButton').setAttribute('style', 'display: inline');
 				    window.location.assign('/auth/signout');
 			    },
 			    error: function(e) {
 			        // Handle the error
 			        console.log(e);
-			        // You could point users to manually disconnect if unsuccessful
-			        // https://plus.google.com/apps
 			    }
 			});
 		}
