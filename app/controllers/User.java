0c5c0a511bc62d6c9c4746b7da19714f4ab76f6f
Luís Loureiro <llorieruo@hotmail.com>
2013-04-01 22:23:55 +0100

    Using a query string parameter instead of a segment in the path part of the URL.

--- app/controllers/User.java
+++ app/controllers/User.java
@@ -8,7 +8,7 @@ import views.html.user.index;
 public class User extends Controller {
 
 	@Authenticated
-	public static Result index(String authProvider) {
-		return ok(index.render(request().username(), authProvider));
+	public static Result index() {
+		return ok(index.render(request().username(), request().getQueryString("provider")));
 	}
 }
