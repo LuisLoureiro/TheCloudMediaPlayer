020d5b83e9614223a7fcc5da44eece373752d159
Luís Loureiro <llorieruo@hotmail.com>
2013-04-05 16:34:37 +0100

    Added a close button to the alert messages.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,5 @@
 function appendAlert(errorType, message) {
-	$('body>.container-fluid').prepend('<div class="row-fluid"><div class="span12"><div class="alert alert-'+errorType+'">'+message+'</div></div></div>');
+	$('body>.container-fluid').prepend('<div class="row-fluid"><div class="span12"><div class="alert alert-'+errorType+'"><a class="close">x</a>'+message+'</div></div></div>');
 }
 function appendErrorAlert(message) {
 	funcAlert("error", message);
@@ -10,3 +10,9 @@ function appendInfoAlert(message) {
 function appendSuccessAlert(message) {
 	funcAlert("success", message);
 }
+$(document).ready(function(){
+	$('a.close').click(function(){
+		// a.close <- div.alert.alert-X <- div.span# <- div.row-fluid
+		$(this).parent().parent().parent().slideUp('slow');
+	});
+});
