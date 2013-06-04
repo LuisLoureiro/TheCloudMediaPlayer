072d8317515e31de22e3aedd0bf3fa817314d480
Luís Loureiro <llorieruo@hotmail.com>
2013-06-04 17:50:27 +0100

    Implementation of the getResources method. Just getting songs from soundcloud user playlists. Using strategy pattern to parse responses.

--- app/controllers/operations/authentication/SoundcloudOAuth2.java
+++ app/controllers/operations/authentication/SoundcloudOAuth2.java
@@ -3,19 +3,20 @@ package controllers.operations.authentication;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Properties;
 
 import models.authentication.AccessToken;
+import models.beans.Resource;
 import models.beans.ServiceResources;
+import models.beans.dataBinding.Playlist;
+import models.beans.dataBinding.Track;
+import models.beans.dataBinding.UserId;
 
 import org.apache.http.HttpResponse;
-import org.codehaus.jackson.JsonFactory;
-import org.codehaus.jackson.JsonParseException;
-import org.codehaus.jackson.JsonParser;
-import org.codehaus.jackson.JsonToken;
 
 import play.i18n.Lang;
 
@@ -25,15 +26,19 @@ import com.soundcloud.api.Endpoints;
 import com.soundcloud.api.Request;
 import com.soundcloud.api.Token;
 
+import controllers.enums.OAUTH_SERVICE_PROVIDERS;
 import controllers.operations.authentication.exceptions.OAuth2ValidationException;
 import controllers.operations.authentication.exceptions.OAuthException;
+import controllers.operations.parsers.IParserStrategy;
 
 public class SoundcloudOAuth2 implements IOAuth2
 {
 	private final String APP_KEY, APP_SECRET;
 	private final ApiWrapper WRAPPER;
+	
+	private final IParserStrategy PARSER;
 
-	public SoundcloudOAuth2(String redirectUrl) throws InstantiationException
+	public SoundcloudOAuth2(IParserStrategy parser, String redirectUrl) throws InstantiationException
 	{
 		try
 		{
@@ -43,6 +48,8 @@ public class SoundcloudOAuth2 implements IOAuth2
 			this.APP_SECRET = properties.getProperty("SOUNDCLOUD_CLIENT_SECRET");
 			
 			this.WRAPPER = new ApiWrapper(APP_KEY, APP_SECRET, new URI(redirectUrl), null);
+			
+			this.PARSER = parser;
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			throw new InstantiationException(e.getMessage());
@@ -65,19 +72,45 @@ public class SoundcloudOAuth2 implements IOAuth2
 		AccessToken uidAccessRefreshToken = new AccessToken();
 		try {
 			Token accessToken = this.WRAPPER.authorizationCode(requestToken);
-//			this.WRAPPER.setToken(accessToken);
 			
 			uidAccessRefreshToken.setAccessToken(accessToken.access);
 			uidAccessRefreshToken.setRefreshToken(accessToken.refresh);
-			// TODO the uid. Send a request to /me and grab the id value!
 			// TODO think about the possibility to use the public uri to be the id. It's unique! Think about it for every service!
-			// TODO use enumerator for the accept header value..setHeader(org.apache.http.HttpHeaders.ACCEPT, "application/json")
+			// TODO beware about the .json and the PARSER.
 			HttpResponse response = this.WRAPPER.get(Request.to(Endpoints.MY_DETAILS+".json").usingToken(accessToken));
-			uidAccessRefreshToken.setUid(getIdFromJSON(response.getEntity().getContent()));
+			uidAccessRefreshToken.setUid(
+					PARSER.parse(UserId.class, response.getEntity().getContent())
+						.getId());
 		} catch (IOException e) {
 			throw new OAuthException(e.getMessage(), e);
 		}
 		return uidAccessRefreshToken;
+	}
+
+	@Override
+	public ServiceResources getResources(AccessToken accessToken)
+	{
+		List<Resource> resourcesList = new LinkedList<Resource>();
+		ServiceResources resources = new ServiceResources(OAUTH_SERVICE_PROVIDERS.SOUNDCLOUD.toString(), resourcesList);
+		try {
+//			HttpResponse resp = this.WRAPPER.get(Request.to(Endpoints.MY_FAVORITES+".json").usingToken(new Token(accessToken.getAccessToken(), accessToken.getRefreshToken())));
+//			HttpResponse resp = this.WRAPPER.get(Request.to(Endpoints.MY_TRACKS+".json").usingToken(new Token(accessToken.getAccessToken(), accessToken.getRefreshToken())));
+//			HttpResponse resp = this.WRAPPER.get(Request.to(Endpoints.MY_EXCLUSIVE_TRACKS+".json").usingToken(new Token(accessToken.getAccessToken(), accessToken.getRefreshToken())));
+			HttpResponse resp = this.WRAPPER.get(Request.to(Endpoints.MY_PLAYLISTS+".json").usingToken(new Token(accessToken.getAccessToken(), accessToken.getRefreshToken())));
+			for(Playlist playlist : PARSER.parse(Playlist[].class, resp.getEntity().getContent()))
+			{
+				for(Track track : playlist.getTracks())
+				{
+					resourcesList.add(new Resource(track.getTitle()));
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			// TODO throw new
+		}
+		
+		return resources;
 	}
 
 	@Override
@@ -90,48 +123,5 @@ public class SoundcloudOAuth2 implements IOAuth2
 	public void validateToken(TokenResponse token, String userId, Lang lang) throws OAuth2ValidationException, IOException {
 		// TODO Auto-generated method stub
 		
-	}
-	
-	// TODO read this: http://wiki.fasterxml.com/JacksonStreamingApi
-	private String getIdFromJSON(InputStream stream)
-	{
-		String value = null;
-		try {
-			JsonFactory jsonFactory = new JsonFactory(); // or, for data binding, org.codehaus.jackson.mapper.MappingJsonFactory 
-			JsonParser jp = jsonFactory.createJsonParser(stream); // or URL, Stream, Reader, String, byte[]
-			// Read this: Also, if you happen to have an ObjectMapper, there is also ObjectMapper.getJsonFactory() that you can use to reuse factory it has
-			//  (since (re)using a JsonFactory instances is one Performance Best Practices).
-			// Sanity check: verify that we got "Json Object":
-			if (jp.nextToken() != JsonToken.START_OBJECT)
-			{
-			    throw new IOException("Expected data to start with an Object");
-			}
-			// Iterate over object fields:
-			while (jp.nextToken() != JsonToken.END_OBJECT)
-			{
-			    String fieldName = jp.getCurrentName();
-			    // Let's move to value
-			    jp.nextToken();
-			    if (fieldName.equals("id")) {
-			    	value = String.valueOf(jp.getLongValue());
-			    	break;
-			    }
-			}
-			jp.close(); // important to close both parser and underlying File reader
-			return value;
-		} catch (JsonParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return value;
-	}
-
-	@Override
-	public ServiceResources getResources() {
-		// TODO Auto-generated method stub
-		return null;
 	}
 }
