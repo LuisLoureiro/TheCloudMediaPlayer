57b5e93c5cbd2d764cf103bc4d08c8ddd952b99f
Luís Loureiro <llorieruo@hotmail.com>
2013-06-13 20:07:02 +0100

    Creating a new wrapper object before making a calling to the API. This version avoids problems with multiuser access.

--- app/controllers/operations/authentication/SoundcloudOAuth2.java
+++ app/controllers/operations/authentication/SoundcloudOAuth2.java
@@ -1,11 +1,9 @@
 package controllers.operations.authentication;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Properties;
@@ -31,7 +29,8 @@ import controllers.operations.parsers.IParserStrategy;
 public class SoundcloudOAuth2 implements IOAuth
 {
 	private final String APP_KEY, APP_SECRET;
-	private final ApiWrapper WRAPPER;
+//	private final ApiWrapper WRAPPER;
+	private final URI redirectUrl;
 	
 	private final IParserStrategy PARSER;
 
@@ -44,13 +43,12 @@ public class SoundcloudOAuth2 implements IOAuth
 			this.APP_KEY = properties.getProperty("SOUNDCLOUD_CLIENT_ID");
 			this.APP_SECRET = properties.getProperty("SOUNDCLOUD_CLIENT_SECRET");
 			
-			this.WRAPPER = new ApiWrapper(APP_KEY, APP_SECRET, new URI(redirectUrl), null);
+//			this.WRAPPER = new ApiWrapper(APP_KEY, APP_SECRET, new URI(redirectUrl), null);
+			this.redirectUrl = new URI(redirectUrl);
 			
 			this.PARSER = parser;
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			throw new InstantiationException(e.getMessage());
 		} catch (IOException | URISyntaxException e) {
+			e.printStackTrace(); // TODO remove!
 			// TODO Auto-generated catch block
 			throw new InstantiationException(e.getMessage());
 		}
@@ -60,7 +58,10 @@ public class SoundcloudOAuth2 implements IOAuth
 	public String getRequestToken(String callbackUrl)
 	{
 		// TODO we should send a state value, confirming it in the callback.
-		return this.WRAPPER.authorizationCodeUrl(Endpoints.CONNECT).toString();
+//		return this.WRAPPER.authorizationCodeUrl(Endpoints.CONNECT).toString();
+		return new ApiWrapper(APP_KEY, APP_SECRET, redirectUrl, null)
+				.authorizationCodeUrl(Endpoints.CONNECT)
+				.toString();
 	}
 
 	@Override
@@ -68,15 +69,20 @@ public class SoundcloudOAuth2 implements IOAuth
 	{
 		AccessToken uidAccessRefreshToken = new AccessToken();
 		try {
-			Token accessToken = this.WRAPPER.authorizationCode(requestToken);
+//			Token accessToken = this.WRAPPER.authorizationCode(requestToken);
+			ApiWrapper wrapper = new ApiWrapper(APP_KEY, APP_SECRET, redirectUrl, null);
+			Token accessToken = wrapper.authorizationCode(requestToken);
 			
 			uidAccessRefreshToken.setAccessToken(accessToken.access);
 			uidAccessRefreshToken.setRefreshToken(accessToken.refresh);
-			uidAccessRefreshToken.setExpiresIn(new Date().getTime()+(accessToken.expiresIn*1000)); // See http://tools.ietf.org/html/draft-ietf-oauth-v2-31#page-40
+			// See http://tools.ietf.org/html/draft-ietf-oauth-v2-31#page-40
+			// The expires in returned in the Token object is the expire date instead of lifetime in seconds as said in the ieft draft.
+			uidAccessRefreshToken.setExpiresIn(accessToken.expiresIn);
 			
 			// TODO think about the possibility to use the public uri to be the id. It's unique! Think about it for every service!
 			// TODO beware about the .json and the PARSER.
-			HttpResponse response = this.WRAPPER.get(Request.to(Endpoints.MY_DETAILS+".json").usingToken(accessToken));
+//			HttpResponse response = this.WRAPPER.get(Request.to(Endpoints.MY_DETAILS+".json").usingToken(accessToken));
+			HttpResponse response = wrapper.get(Request.to(Endpoints.MY_DETAILS+".json"));
 			uidAccessRefreshToken.setUid(
 					PARSER.parse(UserId.class, response.getEntity().getContent())
 						.getId());
@@ -90,12 +96,12 @@ public class SoundcloudOAuth2 implements IOAuth
 	public ServiceResources getResources(AccessToken accessToken)
 	{
 		List<Resource> resourcesList = new LinkedList<Resource>();
-		ServiceResources resources = new ServiceResources(OAUTH_SERVICE_PROVIDERS.SOUNDCLOUD.toString(), resourcesList);
 		try {
+			ApiWrapper wrapper = new ApiWrapper(APP_KEY, APP_SECRET, redirectUrl, new Token(accessToken.getAccessToken(), accessToken.getRefreshToken()));
 //			HttpResponse resp = this.WRAPPER.get(Request.to(Endpoints.MY_FAVORITES+".json").usingToken(new Token(accessToken.getAccessToken(), accessToken.getRefreshToken())));
 //			HttpResponse resp = this.WRAPPER.get(Request.to(Endpoints.MY_TRACKS+".json").usingToken(new Token(accessToken.getAccessToken(), accessToken.getRefreshToken())));
 //			HttpResponse resp = this.WRAPPER.get(Request.to(Endpoints.MY_EXCLUSIVE_TRACKS+".json").usingToken(new Token(accessToken.getAccessToken(), accessToken.getRefreshToken())));
-			HttpResponse resp = this.WRAPPER.get(Request.to(Endpoints.MY_PLAYLISTS+".json").usingToken(new Token(accessToken.getAccessToken(), accessToken.getRefreshToken())));
+			HttpResponse resp = wrapper.get(Request.to(Endpoints.MY_PLAYLISTS+".json"));
 			Playlist[] contents = PARSER.parse(Playlist[].class, resp.getEntity().getContent());
 			if(contents != null)
 			{
@@ -113,6 +119,6 @@ public class SoundcloudOAuth2 implements IOAuth
 			// TODO throw new
 		}
 		
-		return resources;
+		return new ServiceResources(OAUTH_SERVICE_PROVIDERS.SOUNDCLOUD.toString(), resourcesList);
 	}
 }
