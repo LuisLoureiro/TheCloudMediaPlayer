ace4b96927daf0f03a1687236ab2313f7ed71355
Luís Loureiro <llorieruo@hotmail.com>
2013-06-20 22:46:19 +0100

    Registering new functions on click events. These functions are used to play resources from the playlist. Making an AJAX call to the server to get the stream url of the specified resource.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -19,7 +19,34 @@ $(document).ready(function(){
 		$(this).parent().parent().parent().slideUp('slow');
 	});
 	// Append song to play list.
+	// TODO see if it's possible to just copy the anchor tag and add the class playlist-resource
 	$(document).on("click", "#resources-list>ul>li>a", function(){
-		var name=$(this).text(); $('#playlist-table>tbody').append('<tr><td><a href="#">'+name+'</a></td></tr>');
-	})
+		var name=$(this).text();
+		var trackId=$(this).attr('data-track-id');
+		var providerName=$(this).parent().prevAll().filter('.nav-header').first().text();
+		$('#playlist-table>tbody').append('<tr><td><a class="playlist-resource" data-track-id="'+trackId+'" data-provider-name="'+providerName+'" href="#">'+name+'</a></td></tr>');
+	});
+	// Play resource
+	$(document).on("click", ".playlist-resource", function(){
+		var trackId=$(this).attr('data-track-id');
+		var providerName=$(this).attr('data-provider-name');
+		$.ajax({
+			type: 'GET',
+			url: '/track/streamurl/'+encodeURIComponent(trackId),
+			dataType: 'json',
+			data: {
+				'providerName': providerName
+			},
+			success: function(trackUrl) {
+				console.log(trackUrl);
+				$('#playing>audio').html('<source src="'+trackUrl.url+'"></source>');
+			},
+			error: function(jqXHR, textStatus, errorThrown) {
+				// Handle error
+				appendErrorAlert(jqXHR.responseText);
+				console.log(textStatus);
+				console.log(errorThrown);
+			}
+		});
+	});
 });
