d0750d79a7d33a14cdc03b2816cb08002ed230f6
Luís Loureiro <llorieruo@hotmail.com>
2013-07-03 16:01:23 +0100

    Created the HTTP resources for the Playlist controller actions: save, load and delete.

---
+++ app/controllers/Playlist.java
@@ -0,0 +1,22 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class Playlist extends Controller
+{
+	public static Result save(String name)
+	{
+		return status(NOT_IMPLEMENTED);
+	}
+	
+	public static Result load(int id)
+	{
+		return status(NOT_IMPLEMENTED);
+	}
+	
+	public static Result delete(int id)
+	{
+		return status(NOT_IMPLEMENTED);
+	}
+}
