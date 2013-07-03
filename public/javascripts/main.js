5c134a004733c6f34cded29b593161bc4593fbea
Luís Loureiro <llorieruo@hotmail.com>
2013-07-03 16:27:46 +0100

    Splitting code into new functions.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -13,45 +13,48 @@ function appendSuccessAlert(message) {
 function emptyAlerts() {
 	$('#alerts').empty();
 }
-$(document).ready(function(){
-	$(document).on("click", 'a.close', function(){
-		// a.close <- div.alert.alert-X <- div.span# <- div.row-fluid
-		$(this).parent().parent().parent().slideUp('slow');
-	});
-	// Append song to play list.
+function slideUpAlert(elem){
+	// a.close <- div.alert.alert-X <- div.span# <- div.row-fluid
+	$(elem).parent().parent().parent().slideUp('slow');
+}
+function appendSongToPlayList(elem){
+	var name=$(elem).text();
+	var trackId=$(elem).attr('data-track-id');
+	var mimeType=$(elem).attr('data-track-mimetype');
+	var providerName=$(elem).parent().prevAll().filter('.nav-header').first().text().toLowerCase();
 	// TODO see if it's possible to just copy the anchor tag and add the class playlist-resource
-	$(document).on("click", "#resources-list>ul>li>a", function(){
-		var name=$(this).text();
-		var trackId=$(this).attr('data-track-id');
-		var mimeType=$(this).attr('data-track-mimetype');
-		var providerName=$(this).parent().prevAll().filter('.nav-header').first().text().toLowerCase();
-		$('#playlist-table>tbody').append('<tr><td><a class="playlist-resource" data-track-id="'+trackId+'" data-provider-name="'+providerName
-				+'" data-track-mimetype='+mimeType+' href="#">'+name+'</a></td></tr>');
+	$('#playlist-table>tbody').append('<tr><td><a class="playlist-resource" data-track-id="'+trackId+'" data-provider-name="'+providerName
+			+'" data-track-mimetype='+mimeType+' href="#">'+name+'</a></td></tr>');
+}
+function playContent(elem){
+	var trackId=$(elem).attr('data-track-id');
+	var mimeType=$(elem).attr('data-track-mimetype');
+	var providerName=$(elem).attr('data-provider-name');
+	$.ajax({
+		type: 'GET',
+		url: '/track/streamurl/'+encodeURIComponent(trackId),
+		dataType: 'json',
+		data: {
+			'providerName': providerName
+		},
+		success: function(trackUrl) {
+			console.log(trackUrl);
+			console.log(mimeType);
+			$('#playing').html((mimeType.indexOf('audio') != -1) ? '<audio autoplay="autoplay" controls="controls"><source src="'+trackUrl.url+'"></source></audio>'
+					: '<video autoplay="autoplay" controls="controls"><source src="'+trackUrl.url+'" type="'+mimeType+'"></source></video>');
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+			// Handle error
+			appendErrorAlert(errorThrown);
+			console.log(textStatus);
+			console.log(jqXHR.responseText);
+		}
 	});
+}
+$(document).ready(function(){
+	$(document).on("click", 'a.close', function(){slideUpAlert(this);});
+	// Append song to play list.
+	$(document).on("click", "#resources-list>ul>li>a", function(){appendSongToPlayList(this);});
 	// Play resource
-	$(document).on("click", ".playlist-resource", function(){
-		var trackId=$(this).attr('data-track-id');
-		var mimeType=$(this).attr('data-track-mimetype');
-		var providerName=$(this).attr('data-provider-name');
-		$.ajax({
-			type: 'GET',
-			url: '/track/streamurl/'+encodeURIComponent(trackId),
-			dataType: 'json',
-			data: {
-				'providerName': providerName
-			},
-			success: function(trackUrl) {
-				console.log(trackUrl);
-				console.log(mimeType);
-				$('#playing').html((mimeType.indexOf('audio') != -1) ? '<audio autoplay="autoplay" controls="controls"><source src="'+trackUrl.url+'"></source></audio>'
-						: '<video autoplay="autoplay" controls="controls"><source src="'+trackUrl.url+'" type="'+mimeType+'"></source></video>');
-			},
-			error: function(jqXHR, textStatus, errorThrown) {
-				// Handle error
-				appendErrorAlert(errorThrown);
-				console.log(textStatus);
-				console.log(jqXHR.responseText);
-			}
-		});
-	});
+	$(document).on("click", ".playlist-resource", function(){playContent(this);});
 });
