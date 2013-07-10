7a01f740be7115b38471e092a6417950a9933dba
Luís Loureiro <llorieruo@hotmail.com>
2013-07-10 22:41:01 +0100

    Data mapper for the Playlist entity.

---
+++ app/models/mapper/PlaylistMapper.java
@@ -0,0 +1,61 @@
+package models.mapper;
+
+import java.util.Collection;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import models.db.Playlist;
+import models.db.compositeKeys.PlaylistPK;
+import play.db.jpa.JPA;
+
+public class PlaylistMapper implements IMapper<PlaylistPK, Playlist>
+{
+	@Override
+	public Playlist findById(PlaylistPK id)
+	{
+		return JPA.em().find(Playlist.class, id);
+	}
+
+	@Override
+	public Collection<Playlist> findBy(String field, String data)
+	{
+		CriteriaBuilder cbPlaylist = JPA.em().getCriteriaBuilder();
+		CriteriaQuery<Playlist> cqPlaylist = cbPlaylist.createQuery(Playlist.class);
+		Root<Playlist> playlist = cqPlaylist.from(Playlist.class);
+		return JPA.em().createQuery(
+				cqPlaylist.select(playlist)
+					.where(
+						cbPlaylist.equal(playlist.get(field), data)
+					)
+			).getResultList();
+	}
+
+	@Override
+	public Collection<Playlist> getAll()
+	{
+		CriteriaBuilder cbPlaylist = JPA.em().getCriteriaBuilder();
+		CriteriaQuery<Playlist> cqPlaylist = cbPlaylist.createQuery(Playlist.class);
+		return JPA.em().createQuery(
+				cqPlaylist.select(cqPlaylist.from(Playlist.class))
+			).getResultList();
+	}
+
+	@Override
+	public void update(Playlist object) {
+		JPA.em().merge(object);
+	}
+
+	@Override
+	public void save(Playlist object)
+	{
+		JPA.em().persist(object);
+	}
+
+	@Override
+	public void delete(Playlist object)
+	{
+		JPA.em().remove(object);
+	}
+}
