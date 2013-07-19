6ee9b87e84793d9b24204e30456bbc8c02622400
Luís Loureiro <llorieruo@hotmail.com>
2013-07-19 21:54:03 +0100

    Grabbing the contents passed to the save action using form binding.

--- app/models/beans/dataBinding/form/PlaylistForm.java
+++ app/models/beans/dataBinding/form/PlaylistForm.java
@@ -1,5 +1,7 @@
 package models.beans.dataBinding.form;
 
+import java.util.List;
+
 import play.data.validation.Constraints.Required;
 
 public class PlaylistForm
@@ -7,6 +9,8 @@ public class PlaylistForm
 	private long id;
 	@Required(message="form.playlistForm.nameRequired")
 	private String name;
+	@Required(message="form.playlistForm.contentsRequired")
+	private List<Content> contents;
 
 	public long getId() {
 		return id;
@@ -19,5 +23,30 @@ public class PlaylistForm
 	}
 	public void setName(String name) {
 		this.name = name;
+	}
+	public List<Content> getContents() {
+		return contents;
+	}
+	public void setContents(List<Content> contents) {
+		this.contents = contents;
+	}
+	
+	public static class Content
+	{
+		private String id;
+		private String provider;
+		
+		public String getId() {
+			return id;
+		}
+		public void setId(String id) {
+			this.id = id;
+		}
+		public String getProvider() {
+			return provider;
+		}
+		public void setProvider(String provider) {
+			this.provider = provider;
+		}
 	}
 }
