3c6270ee7239b5d9a321f5463c7cd34d67c600d8
Luís Loureiro <llorieruo@hotmail.com>
2013-07-30 21:36:04 +0100

    Using a new postgreSQL database for testing purposes. Using some test values to test application.

--- app/Global.java
+++ app/Global.java
@@ -1,12 +1,24 @@
 import java.lang.reflect.Method;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+
+import javax.persistence.EntityManager;
+
+import models.db.Content;
+import models.db.Playlist;
+import models.db.PlaylistContent;
+import models.db.User;
 
 import org.codehaus.jackson.node.ObjectNode;
 
 import play.Application;
 import play.GlobalSettings;
+import play.db.jpa.JPA;
+import play.libs.F.Callback0;
 import play.libs.Json;
+import play.libs.Yaml;
 import play.mvc.Action;
 import play.mvc.Http.Request;
 import play.mvc.Http.RequestHeader;
@@ -15,29 +27,55 @@ import play.mvc.Results;
 import scala.Tuple2;
 import scala.collection.JavaConversions;
 
-
 public class Global extends GlobalSettings
 {
 	// Intercepting application start-up and shutdown
 	@Override
-	public void onStart(Application app) {
-//	    Logger.info("Application has started");
+	public void onStart(Application app)
+	{
+		// Logger.info("Application has started");
+		if(app.isTest())
+		{
+			final Map<String,List<Object>> all = (Map<String,List<Object>>)Yaml.load("test-data.yml");
+			JPA.withTransaction(new Callback0()
+			{
+				@Override
+				public void invoke() throws Throwable
+				{
+					EntityManager manager = JPA.em();
+					for(Object user : all.get("users"))
+					{
+						User userUser = (User)user;
+						manager.persist(userUser);
+					}
+					for(Object content : all.get("contents"))
+					{
+						Content contentContent = (Content)content;
+						manager.persist(contentContent);
+					}
+				}
+			});
+		}
 		super.onStart(app);
 	}
 	
 	@Override
-	public void onStop(Application app) {
-//	    Logger.info("Application shutdown...");
+	public void onStop(Application app)
+	{
+		// Logger.info("Application shutdown...");
 		super.onStop(app);
 	}
 	
 	// Providing an application error page
-	// When an exception occurs in your application, the onError operation will be called. The default is to use the internal framework error page.
+	// When an exception occurs in your application, the onError operation will
+	// be called. The default is to use the internal framework error page.
 	@Override
-	public Result onError(RequestHeader request, Throwable t) {
-//	    return internalServerError(views.html.error.errorPage.render(t));
-//		return Results.internalServerError(views.html.defaultpages.error.render(null));
-//		return super.onError(request, t);
+	public Result onError(RequestHeader request, Throwable t)
+	{
+		// return internalServerError(views.html.error.errorPage.render(t));
+		// return
+		// Results.internalServerError(views.html.defaultpages.error.render(null));
+		// return super.onError(request, t);
 		
 		// Get error message
 		String errorMessage;
@@ -46,21 +84,30 @@ public class Global extends GlobalSettings
 		{
 			Throwable innerInnerT = innerT.getCause();
 			errorMessage = innerInnerT != null ? innerInnerT.getMessage() : innerT.getMessage();
-		}else
+		}
+		else
 		{
 			errorMessage = t.getMessage();
 		}
+		// TODO catch ConstraintViolationExceptions
+//		catch (ConstraintViolationException e)
+//		{
+//			Set<ConstraintViolation<?>> constraints = e.getConstraintViolations();
+//			for(ConstraintViolation<?> constraint : constraints)
+//				...
+//			assertEquals(constraints.iterator().next().getMessage(), "user.playList.notNull");
+//	    }
 		
 		// Content negotiation
 		if(request.accepts("application/json") || request.accepts("text/json"))
 		{
-		    // Return a json object with the error message
+			// Return a json object with the error message
 			ObjectNode result = Json.newObject();
 			result.put("error", errorMessage);
 			return Results.internalServerError(result);
 		}
 		// If authenticated content = views.html.user.index
-		//  we need to find a way to verify if the user is authenticated or not!
+		// we need to find a way to verify if the user is authenticated or not!
 		
 		// Else content = views.html.application.index
 		Result ret = Results.internalServerError(views.html.application.index.render());
@@ -73,29 +120,41 @@ public class Global extends GlobalSettings
 	}
 	
 	// Handling action not found
-	// If the framework doesn’t find an action method for a request, the onHandlerNotFound operation will be called.
+	// If the framework doesn’t find an action method for a request, the
+	// onHandlerNotFound operation will be called.
 	@Override
-	public Result onHandlerNotFound(RequestHeader request) {
-//	    return Results.notFound(views.html.error.pageNotFound.render(request.uri()));
-//		return Results.notFound(views.html.defaultpages.notFound.render(request, null));
+	public Result onHandlerNotFound(RequestHeader request)
+	{
+		// return
+		// Results.notFound(views.html.error.pageNotFound.render(request.uri()));
+		// return
+		// Results.notFound(views.html.defaultpages.notFound.render(request,
+		// null));
 		return super.onHandlerNotFound(request);
 	}
 	
-	// The onBadRequest operation will be called if a route was found, but it was not possible to bind the request parameters.
+	// The onBadRequest operation will be called if a route was found, but it
+	// was not possible to bind the request parameters.
 	@Override
-	public Result onBadRequest(RequestHeader request, String error) {
-//	    return Results.badRequest(views.html.error.badRequest.render());
-//		return Results.badRequest(views.html.defaultpages.badRequest.render(request, "Don't try to hack the URI!"));
+	public Result onBadRequest(RequestHeader request, String error)
+	{
+		// return Results.badRequest(views.html.error.badRequest.render());
+		// return
+		// Results.badRequest(views.html.defaultpages.badRequest.render(request,
+		// "Don't try to hack the URI!"));
 		return super.onBadRequest(request, error);
 	}
 	
 	// Overriding onRequest
-	// One important aspect of the GlobalSettings class is that it provides a way to intercept requests and execute business logic before a request is dispatched to an action.
+	// One important aspect of the GlobalSettings class is that it provides a
+	// way to intercept requests and execute business logic before a request is
+	// dispatched to an action.
 	@Override
-	public Action onRequest(Request request, Method actionMethod) {
-	   System.out.println("before each request..." + request.toString());
-	   Action action = super.onRequest(request, actionMethod);
-	   System.out.println("after each request..." + request.toString());
-	   return action;
+	public Action onRequest(Request request, Method actionMethod)
+	{
+		System.out.println("before each request..." + request.toString());
+		Action action = super.onRequest(request, actionMethod);
+		System.out.println("after each request..." + request.toString());
+		return action;
 	}
 }
--- test/unit/BaseTest.java
+++ test/unit/BaseTest.java
@@ -1,12 +1,41 @@
 package unit;
 
+import static org.junit.Assert.fail;
+import static play.test.Helpers.fakeApplication;
+import static play.test.Helpers.running;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.rules.TestRule;
 import org.junit.rules.TestWatcher;
 import org.junit.runner.Description;
 
+import play.db.jpa.JPA;
+import play.libs.F.Callback0;
+import play.libs.F.Function0;
+import play.test.FakeApplication;
+
 public abstract class BaseTest
 {
+	public static Map<String, String> settings = new HashMap<String, String>();
+	public static String	firstUserId
+						, secondUserId
+						, firstUserEmail
+						, firstPlaylistName
+						, secondPlaylistName
+						, firstContentId
+						, secondContentId
+						, firstContentProvider
+						, secondContentProvider;
+	public static int	firstPlaylistId
+						, secondPlaylistId;
+	public FakeApplication fakeApp;
+	private RunningFakeAppWithReadOnlyTransaction fakeAppWithReadOnlyTransaction = new RunningFakeAppWithReadOnlyTransaction();
+	private RunningFakeAppWithTransaction fakeAppWithTransaction = new RunningFakeAppWithTransaction();
+	
 	@Rule
 	public TestRule	watcher	= new TestWatcher()
 							{
@@ -14,6 +43,9 @@ public abstract class BaseTest
 								protected void starting(Description description)
 								{
 									System.out.println("BEGIN - " + description.getDisplayName());
+							        
+							        fakeApp = fakeApplication(settings);
+							        
 									super.starting(description);
 								}
 								
@@ -24,4 +56,79 @@ public abstract class BaseTest
 									super.finished(description);
 								}
 							};
+
+	@BeforeClass
+	public static void setUpBeforeClass()
+	{
+		settings.put("db.default.driver", "org.h2.Driver");
+		settings.put("db.default.url", "jdbc:h2:mem:play;MODE=PostgreSQL");
+		
+		// Test with PostgreSQL database!
+//        settings.put("db.default.driver", "org.postgresql.Driver");
+//        settings.put("db.default.url", "jdbc:postgresql://localhost:5432/thecloudmediaplayertestdb");
+//        settings.put("db.default.user", "test");
+//        settings.put("db.default.password", "test");
+//        settings.put("db.default.jndiName", "DefaultDS");
+//        settings.put("jpa.default", "persistenceUnitTest");
+		
+		firstUserId = "firstUser";
+		secondUserId = "secondUser";
+		firstUserEmail = "first@mail.com";
+		firstPlaylistId = 1;
+		secondPlaylistId = 2;
+		firstPlaylistName = "firstPlaylist";
+		secondPlaylistName = "secondPlaylist";
+		firstContentId = "firstContent";
+		secondContentId = "secondContent";
+		firstContentProvider = "dropbox";
+		secondContentProvider = "soundcloud";
+	}
+
+	<R> void runFakeAppWithReadOnlyTransaction(Function0<R> func)
+	{
+		fakeAppWithReadOnlyTransaction.run(func);
+	}
+
+	<R> void runFakeAppWithTransaction(Callback0 callback)
+	{
+		fakeAppWithTransaction.run(callback);
+	}
+	
+	private class RunningFakeAppWithReadOnlyTransaction
+	{
+		public <R> void run(final Function0<R> func)
+		{
+			running(fakeApp, new Runnable()
+			{
+				@Override
+				public void run()
+				{
+					try
+					{
+						JPA.withTransaction("default", true, func);
+					}
+					catch(Throwable e)
+					{
+						e.printStackTrace();
+						fail("Exception occurred: " + e.getMessage());
+					}
+				}
+			});
+		}
+	}
+	
+	private class RunningFakeAppWithTransaction
+	{
+		public void run(final Callback0 callback)
+		{
+			running(fakeApp, new Runnable()
+			{
+				@Override
+				public void run()
+				{
+					JPA.withTransaction(callback);
+				}
+			});
+		}
+	}
 }
