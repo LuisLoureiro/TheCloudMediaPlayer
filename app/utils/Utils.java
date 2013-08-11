dcce5c8c2869406a41dbccbb00e3dde015d7a1ba
Luís Loureiro <llorieruo@hotmail.com>
2013-08-11 17:30:44 +0100

    Added a new utility method and a new utility interface to allow the transformation of an object into another, evaluating a predicate.

--- app/utils/Utils.java
+++ app/utils/Utils.java
@@ -10,9 +10,14 @@ import play.i18n.Messages;
 
 public class Utils
 {
-	public interface ITransform<T, V>
+	public static interface ITransform<T, V>
 	{
 		V transform(T elem);
+	}
+	
+	public static interface IPredicate<V>
+	{
+		boolean evaluate(V elem);
 	}
 	
 	public static <T> String buildMessageFromValidationErrors(Form<T> form, Lang lang)
@@ -33,8 +38,30 @@ public class Utils
 	public static <V, T> List<V> transform(Iterable<T> iter, ITransform<T, V> transform)
 	{
 		List<V> list = new LinkedList<V>();
-		for (T t : iter) {
-			list.add(transform.transform(t));
+		if(iter != null)
+		{
+			for (T t : iter) {
+				list.add(transform.transform(t));
+			}
+		}
+		
+		return list;
+	}
+
+	public static <V, T> List<V> transformWithPredicate(Iterable<T> iter, ITransform<T, V> transform, IPredicate<V> predicate)
+	{
+		if(predicate == null)
+			return transform(iter, transform);
+		
+		List<V> list = new LinkedList<V>();
+		if(iter != null)
+		{
+			for (T t : iter)
+			{
+				V v = transform.transform(t);
+				if(predicate.evaluate(v))
+					list.add(v);
+			}
 		}
 		
 		return list;
