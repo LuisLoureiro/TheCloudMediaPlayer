7adb754ea2a3c4f5242b766ecbc6e93f206832aa
Luís Loureiro <llorieruo@hotmail.com>
2013-08-11 17:46:02 +0100

    New methods: find a content and save it if don't exists in the data storage; Delete from the data storage all the contents that are not in any play list.

--- app/controllers/operations/persistence/PersistContent.java
+++ app/controllers/operations/persistence/PersistContent.java
@@ -1,6 +1,7 @@
 package controllers.operations.persistence;
 
 import java.util.Collection;
+import java.util.List;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
@@ -16,6 +17,8 @@ import models.mapper.IMapper;
 
 public class PersistContent
 {
+	private static final IMapper<ContentKey, Content> CONTENT_MAPPER = new ContentMapper();
+	
 	public static void deleteOrphanContents()
 	{
 		/**
@@ -26,19 +29,39 @@ public class PersistContent
 		 * WHERE pc.content_id is null 
 		 *  AND pc.content_provider is null;
 		 */
-		IMapper<ContentKey, Content> contentMapper = new ContentMapper();
-		CriteriaBuilder query = contentMapper.getQueryBuilder();
+		CriteriaBuilder query = CONTENT_MAPPER.getQueryBuilder();
 		CriteriaQuery<Content> cQuery = query.createQuery(Content.class);
 		Root<Content> from = cQuery.from(Content.class);
 		Join<Content, PlaylistContent> join = from.join("playlists", JoinType.LEFT);
-		Collection<Content> queryResults = contentMapper.executeQuery(
+		Collection<Content> queryResults = CONTENT_MAPPER.executeQuery(
 				cQuery.select(from)
 					.where(query.isNull(join.get("content"))));
 		
 		for(Content content : queryResults)
 		{
-			if(content.getPlaylists().isEmpty())
-				contentMapper.delete(content);
+				CONTENT_MAPPER.delete(content);
+		}
+	}
+	
+	static Content findIfNullSave(String id, String provider)
+	{
+		ContentKey key = new ContentKey(id, provider);
+		Content content = CONTENT_MAPPER.findById(key);
+		if(content == null)
+		{
+			content = new Content(key, null);
+			CONTENT_MAPPER.save(content);
+		}
+		
+		return content;
+	}
+	
+	static void deleteContentsWithoutPlaylist(List<PlaylistContent> contents)
+	{
+		for(PlaylistContent playlistContent : contents)
+		{
+			if(playlistContent.getContent().getPlaylists().isEmpty())
+				CONTENT_MAPPER.delete(playlistContent.getContent());
 		}
 	}
 }
