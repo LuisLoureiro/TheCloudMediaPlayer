41a483bd3d99d80a966e01c5936eb870aabdc7f7
Luís Loureiro <llorieruo@sapo.pt>
2013-12-04 18:02:57 +0000

    Updating dependencies versions.

--- project/Build.scala
+++ project/Build.scala
@@ -14,16 +14,16 @@ object ApplicationBuild extends Build {
     javaJpa,
     filters
     // groupID % artifactID % revision % configuration
-    , "com.google.api-client" % "google-api-client" % "1.16.0-rc"
-    , "com.google.http-client" % "google-http-client-jackson" % "1.16.0-rc"
-    , "com.google.http-client" % "google-http-client-gson" % "1.16.0-rc"
-    , "com.google.apis" % "google-api-services-oauth2" % "v2-rev43-1.16.0-rc"
+    , "com.google.api-client" % "google-api-client" % "1.+"
+    , "com.google.http-client" % "google-http-client-jackson" % "1.+"
+    , "com.google.http-client" % "google-http-client-gson" % "1.+"
+    , "com.google.apis" % "google-api-services-oauth2" % "v2-rev51-1.17.0-rc"
     , "postgresql" % "postgresql" % "9.1-901-1.jdbc4"
-    , "org.eclipse.persistence" % "org.eclipse.persistence.jpa" % "2.4.0"
-    , "org.syncloud" % "dropbox-client" % "1.5.3"
+    , "org.eclipse.persistence" % "org.eclipse.persistence.jpa" % "2.5.+"
+    , "org.syncloud" % "dropbox-client" % "1.5.+"
     // , "com.dropbox.core" % "dropbox-core-sdk" % "[1.6,1.7)"
-    , "com.soundcloud" % "java-api-wrapper" % "1.3.0"
-    , "com.google.apis" % "google-api-services-youtube" % "v3-rev70-1.16.0-rc"
+    , "com.soundcloud" % "java-api-wrapper" % "1.3.+"
+    // , "com.google.apis" % "google-api-services-youtube" % "v3-rev70-1.16.0-rc"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
@@ -39,9 +39,6 @@ object ApplicationBuild extends Build {
       },
       resolvers += (
           "Google api services" at "http://google-api-client-libraries.appspot.com/mavenrepo"
-      ),
-      resolvers += (
-          "EclipseLink JPA" at "http://download.eclipse.org/rt/eclipselink/maven.repo"
       )
   )
 
