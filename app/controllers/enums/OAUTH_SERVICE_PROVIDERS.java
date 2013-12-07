dd0b40484c697c8a48bbc6a336fd943eacd62c3f
Luís Loureiro <llorieruo@sapo.pt>
2013-12-07 23:57:59 +0000

    Supply methods to get the OAuth providers names.

--- app/controllers/enums/OAUTH_SERVICE_PROVIDERS.java
+++ app/controllers/enums/OAUTH_SERVICE_PROVIDERS.java
@@ -1,27 +1,48 @@
 package controllers.enums;
 
-public enum OAUTH_SERVICE_PROVIDERS {
+import java.util.Arrays;
+import java.util.List;
 
+public enum OAUTH_SERVICE_PROVIDERS
+{
 	DROPBOX("dropbox", "Dropbox")
-	, SOUNDCLOUD("soundcloud", "SoundCloud")
-//	, YOUTUBE("youtube", "YouTube")
-//	, CLOUDPT("cloudpt", "CloudPT")
+		, SOUNDCLOUD("soundcloud", "SoundCloud")
+//		, YOUTUBE("youtube", "YouTube")
+//		, CLOUDPT("cloudpt", "CloudPT")
 	;
 	
-	private final String lowerCase
-		, bestCase;
+	private final String	lowerCase
+							, bestCase;
 	
-	private OAUTH_SERVICE_PROVIDERS(String lowerCase, String bestCase) {
+	private OAUTH_SERVICE_PROVIDERS(String lowerCase, String bestCase)
+	{
 		this.lowerCase = lowerCase;
 		this.bestCase = bestCase;
 	}
 	
-	public String getBestCase() {
+	public String getLowerCase()
+	{
+		return lowerCase;
+	}
+	
+	public String getBestCase()
+	{
 		return bestCase;
 	}
 	
 	@Override
-	public String toString() {
+	public String toString()
+	{
 		return lowerCase;
+	}
+	
+	public static List<String> getAllLowerCase()
+	{
+		return Arrays.asList(DROPBOX.lowerCase, SOUNDCLOUD.lowerCase);
+	}
+	
+	public static List<String> getAllBestCase()
+	{
+		return Arrays.asList(DROPBOX.bestCase, SOUNDCLOUD.bestCase);
 	}
 }
