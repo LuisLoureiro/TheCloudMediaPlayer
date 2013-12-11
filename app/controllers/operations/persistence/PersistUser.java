4023462a1fb7986587110ae673dde88e9fba82f2
Luís Loureiro <llorieruo@sapo.pt>
2013-12-11 14:33:47 +0000

    New User persistence operation: deleting user account; remove all the related OAuth services registries.

--- app/controllers/operations/persistence/PersistUser.java
+++ app/controllers/operations/persistence/PersistUser.java
@@ -1,11 +1,17 @@
 package controllers.operations.persistence;
 
+import java.util.List;
+
 import models.database.User;
+import models.database.notEntity.OAuthUser;
 import models.mapper.IMapper;
 import models.mapper.UserMapper;
+import controllers.enums.OAUTH_SERVICE_PROVIDERS;
+import controllers.operations.exceptions.ApplicationOperationException;
 
-public class PersistUser {
-
+public class PersistUser
+{
+	private static final IMapper<String, User> MAPPER = new UserMapper();
 	/**
 	 * Create a new {@link User} if the {@code userId} is inexistent.
 	 * Before saving the {@link User} check the existence of an {@link User} with email address equal to the {@code userEmail} parameter, 
@@ -16,15 +22,14 @@ public class PersistUser {
 	 */
 	public static void saveUser(String userId, String userEmail)
 	{
-		IMapper<String, User> mapper = new UserMapper(); // TODO static object.
-		User user = mapper.findById(userId);
+		User user = loadUser(userId);
 		if(user == null)
 		{
 			// if there's an user with the same email address...
 			User existingRecord = null;
 			if(userEmail != null && !userEmail.isEmpty())
 			{
-				for (User auxUser : mapper.findBy("email", userEmail)) {
+				for (User auxUser : MAPPER.findBy("email", userEmail)) {
 					existingRecord = auxUser;
 					break;
 				}
@@ -35,7 +40,49 @@ public class PersistUser {
 			user.setEmail(userEmail);
 			user.setFirstAuth(existingRecord);
 			
-			mapper.save(user);
+			MAPPER.save(user);
 		}
+	}
+	
+	public static void deleteUser(String userId) throws ApplicationOperationException
+	{
+		User user = loadUser(userId)
+				, firstAuth;
+		List<String> allLowerCase = OAUTH_SERVICE_PROVIDERS.getAllLowerCase();
+		
+		if(user == null)
+		{
+			throw new ApplicationOperationException("user.userIdUnexistent");
+		}
+		if((firstAuth = user.getFirstAuth()) != null)
+		{
+			firstAuth.removeUser(user);
+		}
+		// TODO this operation should be done automatically with cascade instructions?!? Change the database schema.
+		for(User relatedOAuthUser : PersistOAuthUser.findAllAccessTokens(userId)) // Only remove the services to whom the user has connected to.
+		{
+			/**
+			 * Because of the possibility of relationship between different authentication users (with email address)
+			 * there could be relatedAuth that are not OAuthUser (could be openID) or could be OAuth but from a service
+			 * that we don't want to get contents from.
+			 */
+//			try{
+//				tokens.add((OAuthUser)relatedUser);
+//			} catch(ClassCastException e){/* Not OAuth user! */}
+			System.out.println("Checking relatedOAuthUser: "+relatedOAuthUser.getId());
+			if(relatedOAuthUser instanceof OAuthUser && allLowerCase.contains(((OAuthUser) relatedOAuthUser).getProviderName()))
+			{
+				System.out.println("Removing relatedOAuthUser: "+relatedOAuthUser.getId()+", "+((OAuthUser) relatedOAuthUser).getProviderName());
+				MAPPER.delete(relatedOAuthUser);
+			}
+		}
+
+		System.out.println("Removing user: "+user.getId());
+		MAPPER.delete(user);
+	}
+	
+	static User loadUser(String userId)
+	{
+		return MAPPER.findById(userId);
 	}
 }
